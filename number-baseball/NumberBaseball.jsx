1fe23c51f232149cd8191ac6d2147ff41c73b6a0
Sunghwan Steven Yoo <ysh1096@gmail.com>
2020-09-18 00:01:54 +1000

    Add files via upload

---
+++ number-baseball/NumberBaseball.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import Try from './Try';
+
+function getNumbers() {
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i++) {
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
+}
+
+const NumberBaseball = () => {
+  const [result, setResult] = useState('');
+  const [value, setValue] = useState('');
+  const [answer, setAnswer] = useState(getNumbers());
+  const [tries, setTries] = useState([]);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (value === answer.join('')) {
+      setResult('Homerun!');
+      setTries((prevTries) => {
+        return [...prevTries, { try: value, result: 'Homerun!' }];
+      });
+      alert('Restart Game!');
+      setValue('');
+      setAnswer(getNumbers());
+      setTries([]);
+    } else {
+      const answerArray = value.split('').map((v) => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+      if (tries.length >= 9) {
+        setResult(`Failed 10 times! The answer is ${answer.join(',')}`);
+        alert('Retry!');
+        setValue('');
+        setAnswer(getNumbers());
+        setTries([]);
+      } else {
+        for (let i = 0; i < 4; i++) {
+          if (answerArray[i] === answer[i]) {
+            strike += 1;
+          } else if (answer.includes(answerArray[i])) {
+            ball += 1;
+          }
+        }
+        setTries((prevTries) => {
+          return [...prevTries, { try: value, result: `${strike}Strike ${ball} Ball` }];
+        });
+        setValue('');
+      }
+    }
+  };
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>{result}</h1>
+      <form onSubmit={onSubmitForm}>
+        <input maxLength={4} value={value} onChange={onChangeInput} />
+      </form>
+      <div>Tries: {tries.length}</div>
+      <ul>
+        {tries.map((v, i) => {
+          return <Try key={`${i + 1}turn:`} tryInfo={v} index={i} />;
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default NumberBaseball;
